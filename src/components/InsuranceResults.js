81d0d7c19d61d38adede8e139944c13cb1edfe3f
Katrina Rodriguez <karodriguez8@gmail.com>
2017-02-08 13:15:06 -0500

    adding hard-coded data for insurance results and styling

--- src/components/InsuranceResults.js
+++ src/components/InsuranceResults.js
@@ -1,20 +1,34 @@
 import React, { Component } from 'react'
 import { Link, browserHistory } from 'react-router'
+import InsurancePlans from '../utils/InsurancePlans'
 
 class InsuranceResults extends Component {
 
 	constructor(props){
 		super(props)
+		this.insuranceResults = InsurancePlans
+		this.state = {
+			opacitySetting: 0
+		}
 	}
 
 	componentDidMount(){
-		console.log("Questionnaire2 componentDidMount")
+		this.setState({opacitySetting: 1})
 	}
 
 	render(){
+		var {opacitySetting} = this.state
+		var plansArray = this.insuranceResults.insurancePlans
+		var insuranceResultsList = plansArray.map(function(result, i){
+			return(
+				<div key={i} style={{margin: "5px"}} className="button">
+					<p style={{fontSize: '10px'}}>{result.company}: {result.plan} | ${result.premium}/month</p>
+				</div>
+			)
+		})
 		return(
 			<div>
-				<article id="work" className="panel secondary">
+				<article id="work" className="panel secondary" style={{opacity: opacitySetting, transitionProperty: "opacity", transitionDuration: "1.25s"}}>
 					<div className="image">
 						<img src="/images/sittingdog.png" alt="" data-position="center center" />
 					</div>
@@ -24,10 +38,10 @@ class InsuranceResults extends Component {
 						</ul>
 						<div className="inner">
 							<header>
-								<h2>Insurance Plan Suggestions</h2>
+								<h2>Plans for you</h2>
 							</header>
-							<p>Plans for you</p>
-							<Link to="/signup" className="button">Save for Later</Link>
+							{insuranceResultsList}
+							<Link to="/signup" style={{margin: "25px"}} className="button">Save for Later</Link>
 						</div>
 					</div>
 				</article>
---
+++ src/utils/InsurancePlans.js
@@ -0,0 +1,7 @@
+export default {
+  insurancePlans: [
+    {"company": "Embrace", "plan": "Budget Conscious", "premium": "13.69"},
+    {"company": "Healthy Paws", "plan": "Dog & Puppy Insurance", "premium": "18.89"},
+    {"company": "Pet First", "plan": "Core Basic", "premium": "24.95"},
+  ]
+}
