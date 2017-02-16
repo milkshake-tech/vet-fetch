01d3ed8717137ce6f8c4ae4e09efc8080c71e06f
Katrina Rodriguez <karodriguez8@gmail.com>
2017-02-15 23:52:07 -0500

    capitalizing UserController filepath

--- routes/account.js
+++ routes/account.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var userController = require('../controllers/userController');
+var userController = require('../controllers/UserController');
 var bcrypt = require('bcryptjs')
 
 router.get('/:action', function(req, res, next){
