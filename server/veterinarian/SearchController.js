5a523fd09d1d35447f65b48b3d706ca9cd226ac9
brian <briancorrea@gmail.com>
2017-06-08 12:46:39 -0400

    refactoring get request into promise

--- server/veterinarian/SearchController.js
+++ server/veterinarian/SearchController.js
@@ -1,30 +1,31 @@
-var superagent = require('superagent')
+const superagent = require('superagent')
 
 module.exports = {
+	get: (params, isRaw) => {
+		return new Promise((resolve, reject) => {
+			const endpoint = 'https://api.foursquare.com/v2/venues/explore?client_id='+process.env.FOURSQUARE_client_id+'&client_secret='+process.env.FOURSQUARE_client_secret
+			superagent
+				.get(endpoint)
+				.query({'query': 'Veterinarian'})
+				.query({'near': params.zipcode})
+				.query({'v': '20170504'})
+				.query({'limit': 10})
+				.query({'offset': params.offset})
+				.end((err, res) => {
+					if (err){
+						return reject(err)
+					}
 
-	get: function(params, isRaw, completion){
-		var endpoint = 'https://api.foursquare.com/v2/venues/explore?client_id='+process.env.FOURSQUARE_client_id+'&client_secret='+process.env.FOURSQUARE_client_secret
-		superagent
-			.get(endpoint)
-			.query({'query': 'Veterinarian'})
-			.query({'near': params.zipcode})
-			.query({'v': '20170504'})
-			.query({'limit': 10})
-			.query({'offset': params.offset})
-			.end(function(err, res){
-				if (err){
-					completion(err, null)
-					return
-				}
-				var searchResults = JSON.parse(res.text)
+					let searchResults = JSON.parse(res.text)
 
-				var searchResultItems = searchResults.response.groups[0].items
-				searchResultItems.forEach(function(item, i){
-					delete item.reasons
+					let searchResultItems = searchResults.response.groups[0].items
+					searchResultItems.forEach((item, i) => {
+						delete item.reasons
+					})
+
+					let vetResults = {offset: params.offset, totalResults: searchResults.response.totalResults, veterinarians: searchResultItems, zipcode: params.zipcode}
+					return resolve(vetResults)
 				})
-				var vetResults = {offset: params.offset, totalResults: searchResults.response.totalResults, veterinarians: searchResultItems, zipcode: params.zipcode}
-				completion(null, vetResults)
-				return
-			})
+		})
 	}
 }
